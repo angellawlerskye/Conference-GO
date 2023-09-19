54f576b35cb6be8b92dc3513e37a481cc8c08483
Angel Lawler Skye <angellawlerskye@Gmail.com>
2023-09-18 19:20:06 -0600

    updated attendees/api_views.py to complete api_show_attendee function and confirmed correct data shows up when I send a GET request to http://localhost:8000/api/attendees/10/ using Insomia.

--- attendees/api_views.py
+++ attendees/api_views.py
@@ -55,4 +55,16 @@ def api_show_attendee(request, id):
         }
     }
     """
-    return JsonResponse({})
+    attendee = Attendee.objects.get(id=id)
+    return JsonResponse(
+        {
+            "email": attendee.email,
+            "name": attendee.name,
+            "company_name": attendee.company_name,
+            "created": attendee.created,
+            "conference": {
+                "name": attendee.conference.name,
+                "href": attendee.conference.get_api_url(),
+            },
+        }
+    )
