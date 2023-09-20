9081005fb14d9358964ffc551a0b9eed80ce867b
Angel Lawler Skye <angellawlerskye@Gmail.com>
2023-09-19 22:55:33 -0600

    edited attendees/views.py to add import statements for ModelEncoder and ConferenceListEncoder, created AttendeeListEncoder and AttendeeDetailEncoder, changed api_list_attendee and api_show_attendee to versions that use their respective encoders, ensured enpoints for show attendee details and list attendees returned the expected corresponding Model property data.

--- attendees/api_views.py
+++ attendees/api_views.py
@@ -1,6 +1,15 @@
+from common.json import ModelEncoder
+
 from django.http import JsonResponse
 
 from .models import Attendee
+
+from events.api_views import ConferenceListEncoder
+
+
+class AttendeeListEncoder(ModelEncoder):
+    model = Attendee
+    properties = ["name"]
 
 
 def api_list_attendees(request, conference_id):
@@ -23,6 +32,12 @@ def api_list_attendees(request, conference_id):
         ]
     }
     """
+
+    """
+    Angel's Notes
+    commenting out old api_list_attendee
+    function in favor of one that uses new
+    AttendeeListEncoder
     response = []
     attendees = Attendee.objects.all()
     for attendee in attendees:
@@ -33,6 +48,26 @@ def api_list_attendees(request, conference_id):
             }
         )
     return JsonResponse({"attendees": response})
+    """
+
+    attendees = Attendee.objects.all()
+    return JsonResponse(
+        {"attendees": attendees},
+        encoder=AttendeeListEncoder,
+    )
+
+
+class AttendeeDetailEncoder(ModelEncoder):
+    model = Attendee
+    properties = [
+        "email",
+        "name",
+        "company_name",
+        "created",
+    ]
+    encoders = {
+        "conference": ConferenceListEncoder(),
+    }
 
 
 def api_show_attendee(request, id):
@@ -55,6 +90,12 @@ def api_show_attendee(request, id):
         }
     }
     """
+
+    """
+    Angel's Notes
+    commenting out old api_show_attendee function
+    for new version that uses AttendeeDetailEncoder
+
     attendee = Attendee.objects.get(id=id)
     return JsonResponse(
         {
@@ -67,4 +108,12 @@ def api_show_attendee(request, id):
                 "href": attendee.conference.get_api_url(),
             },
         }
+    )
+    """
+
+    attendee = Attendee.objects.get(id=id)
+    return JsonResponse(
+        attendee,
+        encoder=AttendeeDetailEncoder,
+        safe=False,
     )
